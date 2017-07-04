c47af8035646d9b40733ad130574ca65fb6741a3
zzd <735640642@qq.com>
2017-07-04 16:42:15 +0800

    zzd0704

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by zzd on 2017/7/4.
+//  Copyright © 2017年 zzd. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    for(int i=0;i<10;i++){
+    printf("Hello, World!\n");
+    }
+    return 0;
+}
